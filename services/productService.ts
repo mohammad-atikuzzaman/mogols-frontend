09b1115ccdd9bb9496202ed5bf1a384f8c0d9e9b
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-12-20 02:00:55 +0600

    api endpoint updated on env

--- services/productService.ts
+++ services/productService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:7000';
+const API_URL = process.env.API_URL || 'http://localhost:7000';
 
 const getProducts = async (keyword = '') => {
     const response = await axios.get(`${API_URL}/api/products?keyword=${keyword}`);
--- services/userService.ts
+++ services/userService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:7000';
+const API_URL = process.env.API_URL || 'http://localhost:7000';
 
 const config = () => {
     return {
